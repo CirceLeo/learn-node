affa9b47b6439f7ae2c1c1f46c7a917a4718467f
Calypso Leonard <calypsorleonard@gmail.com>
2022-10-03 15:22:32 -0400

    actually adding data from front

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const http = require('http')  
 const fs = require('fs') 
+const { parse } = require('path')
 
-// function requestListen(req, res){
 
-// }
 
 const serve = http.createServer((req, res) => {
     const url = req.url
@@ -11,12 +10,20 @@ const serve = http.createServer((req, res) => {
     if(url === '/'){
         res.write('<html>')
         res.write('<head><title>Hi</title></head>')
-        res.write('<body><h1 style="color:red">claire i love you</h1><form action="/message" method="POST"><input type="number"><button type="submit">submit</botton></form></body>')
+        res.write('<body style="background-color:blue"><h1 style="color:red">claire i love you</h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">submit</botton></form></body>')
         res.write('</html>')
         return res.end()
     }
     if(url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'I love you shrek')
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsed = Buffer.concat(body).toString()
+            const message = parsed.split('=')[1]
+            fs.writeFileSync('message.txt', 'I love you shrek ' + message)
+        })
         res.statusCode = 302
         res.setHeader('Location', '/')
         // res.write('<html>')
@@ -28,7 +35,7 @@ const serve = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>Hi</title></head>')
-    res.write('<body><h1>wow it me</h1></body>')
+    res.write('<body style="background-color:blue"><h1>wow it me</h1></body>')
     res.write('</html>')
     // res.write('')
 })
