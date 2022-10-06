cfb52f626f5acecf315d1c1b28812b26b546ee2d
Calypso Leonard <calypsorleonard@gmail.com>
2022-10-06 17:18:30 -0400

    adding more 3rd party

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const http = require('http')  
+const express = require('express')
+const parser = require('body-parser')
 const { parse } = require('path')
 
 const routes = require('./routes')
 
-const serve = http.createServer(routes)
+const app = express()
 
-serve.listen(3000)
+app.use(parser.urlencoded({extended:false}))
+
+app.use('/hi', (req, res, next) => {
+    console.log("doing the thing")
+    res.send('<h1>how im here</h1>')
+})
+app.use('/new-fun', (req, res, next) => {
+    res.send('<form action="POST"><input type="text" name="newbie"><button type="sumbit">add a thing</button></form>')
+})
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>i did it</h1>')
+})
+
+app.listen(3000)
