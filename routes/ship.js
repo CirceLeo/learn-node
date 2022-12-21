acd87402a356fb7d03e1a360db52b9e4c81f79e0
Calypso Leonard <calypsorleonard@gmail.com>
2022-12-21 13:10:42 -0500

    trying out handlebars

--- routes/ship.js
+++ routes/ship.js
@@ -13,7 +13,8 @@ router.get('/hi', (req, res, next) => {
 
 router.get('/', (req, res, next) => {
     const prods = adminData.products
-    res.render('shop', {prods: prods, pageTitle: 'shop', path: '/'})
+    res.render('shop', {prods: prods, pageTitle: 'shop', path: '/', hasProducts: prods.length > 0})
+    // res.render('shop', {prods: prods, pageTitle: 'shop', path: '/'})
     // console.log(adminData.products)
     // res.sendFile(path.join(rootDir, 'views', 'base.html'))
 })
